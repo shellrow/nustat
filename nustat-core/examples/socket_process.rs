bc89b80ee01f2342cc87f4df30bfb01b97ffa189
shellrow <81893184+shellrow@users.noreply.github.com>
2024-02-06 00:45:41 +0900

    Update socket_process.rs

--- nustat-core/examples/socket_process.rs
+++ nustat-core/examples/socket_process.rs
@@ -1,9 +1,14 @@
 use nustat_core::socket::SocketInfoOption;
 
 fn main() {
+    let local_ip_map = nustat_core::net::interface::get_local_ip_map();
     let socket_info = nustat_core::socket::get_sockets_info(SocketInfoOption::default());
     for info in socket_info {
         let process_info = info.process.unwrap();
-        println!("[{}][{}][{:?}] {}:{} -> {:?}:{:?}", process_info.pid, process_info.name, info.protocol, info.local_ip_addr, info.local_port, info.remote_ip_addr, info.remote_port);
+        let mut interface_name = "unknown";
+        if let Some(name) = local_ip_map.get(&info.local_ip_addr) {
+            interface_name = name;
+        }
+        println!("[{}][{}][{}][{:?}] {}:{} -> {:?}:{:?}", process_info.pid, process_info.name, interface_name, info.protocol, info.local_ip_addr, info.local_port, info.remote_ip_addr, info.remote_port);
     }
 }
